e761f3963f0cb219d86a5cdf0f95145547082b00
SunRais <luca.rainieri@student.unife.it>
2019-01-21 16:13:08 +0100

    smtp a mano

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,15 +59,18 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
-  ActionMailer::Base.smtp_settings = {
-    :user_name => 'LucaMail',
-    :password => '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',
-    :domain => 'uniutil.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         ENV['MAIL_HOST'],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
 
