eb7aa432df6d0f3f289d0eb5db6623fb9adfb9e1
SunRais <luca.rainieri@student.unife.it>
2019-01-25 15:20:18 +0100

    prova

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/images/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   attr_accessor :code
   has_and_belongs_to_many :subjects
