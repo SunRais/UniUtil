c17855204becaa46260b188faf1a6a6bbe2b5d61
SunRais <luca.rainieri@student.unife.it>
2019-01-25 16:39:30 +0100

    prova

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -16,13 +16,13 @@ class Subject < ApplicationRecord
   end
 
   def self.not_followed_subjects_with_search(search, user_id)
-  	sql = "select s.id, s.name as subject, u.name, u.surname from subjects as s join users on subjects.user_id = users.id where subjects.name like '%" + search.to_s + "%' and subjects.id not in (select subject_id from subjects_users where user_id = " + user_id.to_s + ") order by subjects.name"
+  	sql = "select s.id, s.name as subject, u.name, u.surname from subjects as s join users as u on s.user_id = u.id where s.name like '%" + search.to_s + "%' and s.id not in (select subject_id from subjects_users where user_id = " + user_id.to_s + ") order by s.name"
   	result = ActiveRecord::Base.connection.execute(sql)
     return result
   end
 
   def self.not_followed_subjects_with_course(course_id, user_id)
-  	sql = "select s.id, s.name as subject, u.name, u.surname from subjects as s join users on subjects.user_id = users.id where subjects.id in (select subject_id from courses_subjects where course_id = " + course_id.to_s + ") and subjects.id not in (select subject_id from subjects_users where user_id = " + user_id.to_s + ") order by subjects.name"
+  	sql = "select s.id, s.name as subject, u.name, u.surname from subjects as s join users as u on s.user_id = u.id where s.id in (select subject_id from courses_subjects where course_id = " + course_id.to_s + ") and s.id not in (select subject_id from subjects_users where user_id = " + user_id.to_s + ") order by s.name"
   	result = ActiveRecord::Base.connection.execute(sql)
     return result
   end
