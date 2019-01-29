0af50000926c663940fe0cedfabbf12e28dd9706
SunRais <luca.rainieri@student.unife.it>
2019-01-29 11:09:18 +0100

    prova

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 #Utenti
-user1 = User.new(id: 1, email: "mauro.tortonesi@unife.it", encrypted_password: "$2a$11$v.RifFAq38Cb/hLHj.Zxue6a8CvABPYKeZa/9HBKBp/e2E2VmXjmC", name: "Mauro", surname: "Tortonesi", sex: "M", address: "via Garibaldi 23", city: "Ferrara", province: "Ferrara", telephone_number: "0532881234", is_admin: "false", is_professor: "true", badge_number: "0", remember_created_at: "2018-09-29 13:06:27", confirmed_at: "2018-09-29 13:06:22")
+user1 = User.new(id: 91, email: "mauro.tortonesi@unife.it", encrypted_password: "$2a$11$v.RifFAq38Cb/hLHj.Zxue6a8CvABPYKeZa/9HBKBp/e2E2VmXjmC", name: "Mauro", surname: "Tortonesi", sex: "M", address: "via Garibaldi 23", city: "Ferrara", province: "Ferrara", telephone_number: "0532881234", is_admin: "false", is_professor: "true", badge_number: "0", remember_created_at: "2018-09-29 13:06:27", confirmed_at: "2018-09-29 13:06:22")
 user1.save!(validate: false)
-user2 = User.new(id: 2, email: "cesare.stefanelli@unife.it", encrypted_password: "$2a$11$v.RifFAq38Cb/hLHj.Zxue6a8CvABPYKeZa/9HBKBp/e2E2VmXjmC", name: "Cesare", surname: "Stefanelli", sex: "M", address: "via Garibaldi 24", city: "Ferrara", province: "Ferrara", telephone_number: "333222333", is_admin: "false", is_professor: "true", badge_number: "0", remember_created_at: "2018-09-29 13:06:27", confirmed_at: "2018-09-29 13:06:22")
+user2 = User.new(id: 92, email: "cesare.stefanelli@unife.it", encrypted_password: "$2a$11$v.RifFAq38Cb/hLHj.Zxue6a8CvABPYKeZa/9HBKBp/e2E2VmXjmC", name: "Cesare", surname: "Stefanelli", sex: "M", address: "via Garibaldi 24", city: "Ferrara", province: "Ferrara", telephone_number: "333222333", is_admin: "false", is_professor: "true", badge_number: "0", remember_created_at: "2018-09-29 13:06:27", confirmed_at: "2018-09-29 13:06:22")
 user2.save!(validate: false)
-user3 = User.new(id: 3, email: "marco.gavanelli@unife.it", encrypted_password: "$2a$11$v.RifFAq38Cb/hLHj.Zxue6a8CvABPYKeZa/9HBKBp/e2E2VmXjmC", name: "Marco", surname: "Gavanelli", sex: "M", address: "via Garibaldi 25", city: "Ferrara", province: "Ferrara", telephone_number: "0345123432", is_admin: "false", is_professor: "true", badge_number: "0", remember_created_at: "2018-09-29 13:06:27", confirmed_at: "2018-09-29 13:06:22")
+user3 = User.new(id: 93, email: "marco.gavanelli@unife.it", encrypted_password: "$2a$11$v.RifFAq38Cb/hLHj.Zxue6a8CvABPYKeZa/9HBKBp/e2E2VmXjmC", name: "Marco", surname: "Gavanelli", sex: "M", address: "via Garibaldi 25", city: "Ferrara", province: "Ferrara", telephone_number: "0345123432", is_admin: "false", is_professor: "true", badge_number: "0", remember_created_at: "2018-09-29 13:06:27", confirmed_at: "2018-09-29 13:06:22")
 user3.save!(validate: false)
 
 # Corsi di Studio
@@ -12,13 +12,13 @@ Course.create(name: "Laurea in Ingegneria Elettronica e Informatica", typology: 
 Course.create(name: "Laurea in Ingegneria Civile", typology: "triennale", description: "Il corso di studi in ingegneria civile e ambientale mira a fornire una solida preparazione nelle materie di base dell'ingegneria civile, ponendo attenzione agli aspetti ambientali. Viene infatti data grande importanza all'area matematica e fisica e quindi alle materie fondanti l'ingegneria civile, quali la rappresentazione grafica e il rilievo, l'idraulica, la fisica tecnica, la scienza delle costruzioni, l'architettura tecnica, la topografia, senza perdere di vista aspetti legati al valore economico degli immobili.")
 
 # Materie
-Subject.create(id: 1, name: "Sistemi Operativi", cfu: "6", year: "2", semester: "1", description: "a", duration: "24", user_id: "2")
-Subject.create(id: 2, name: "Reti di Calcolatori", cfu: "6", year: "3", semester: "1",  description: "a", duration: "24", user_id: "1" )
-Subject.create(id: 3, name: "Sistemi Distribuiti e Mobili", cfu: "6", year: "1", semester: "1", description: "a", duration: "24", user_id: "2")
-Subject.create(id: 4, name: "Constraint Programming", cfu: "6", year: "1", semester: "2", description: "a", duration: "24", user_id: "3")
-Subject.create(id: 5, name: "Linguaggi e Traduttori", cfu: "6", year: "2", semester: "1", description: "a", duration: "24", user_id: "3")
-Subject.create(id: 6, name: "Laboratorio di Programmazione Concorrente", cfu: "3", year: "1", semester: "1", description: "a", duration: "24", user_id: "1")
-Subject.create(id: 7, name: "Fondamenti di Informatica Modulo A", cfu: "3", year: "1", semester: "1", description: "a", duration: "24", user_id: "3")
+Subject.create(id: 1, name: "Sistemi Operativi", cfu: "6", year: "2", semester: "1", description: "a", duration: "24", user_id: "92")
+Subject.create(id: 2, name: "Reti di Calcolatori", cfu: "6", year: "3", semester: "1",  description: "a", duration: "24", user_id: "91" )
+Subject.create(id: 3, name: "Sistemi Distribuiti e Mobili", cfu: "6", year: "1", semester: "1", description: "a", duration: "24", user_id: "92")
+Subject.create(id: 4, name: "Constraint Programming", cfu: "6", year: "1", semester: "2", description: "a", duration: "24", user_id: "93")
+Subject.create(id: 5, name: "Linguaggi e Traduttori", cfu: "6", year: "2", semester: "1", description: "a", duration: "24", user_id: "93")
+Subject.create(id: 6, name: "Laboratorio di Programmazione Concorrente", cfu: "3", year: "1", semester: "1", description: "a", duration: "24", user_id: "91")
+Subject.create(id: 7, name: "Fondamenti di Informatica Modulo A", cfu: "3", year: "1", semester: "1", description: "a", duration: "24", user_id: "93")
 
 # Tabella di giunzione tra corso di studio e materia
 CoursesSubject.create(course_id: "2", subject_id: "1")
