713cf4bc784ee4f2d0a0737639636e2acb5ee69d
SunRais <luca.rainieri@student.unife.it>
2019-02-04 13:54:19 +0100

    aggiornati i seed e aggiunte validazioni

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ApplicationRecord
   has_many :comments
   has_many :subjects
   has_many :notifications, foreign_key: :recipient_id
+  
   # Include default devise modules. Others available are: :lockable, :timeoutable 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, :confirmable 
+
+  validates :name, presence: true, allow_blank: false
+  validates :surname, presence: true, allow_blank: false
+  validates :badge_number, presence: true, allow_blank: false
+  validates :address, presence: true, allow_blank: false
+  validates :city, presence: true, allow_blank: false
+  validates :province, presence: true, allow_blank: false
+  validates :telephone_number, presence: true, allow_blank: false
+
 end
